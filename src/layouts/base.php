b5301247517050259c2d22674077490237fbc7f5
KairuChaaan <101082963+KyleGhats@users.noreply.github.com>
2024-01-12 16:02:47 +0800

    added meta file

--- src/layouts/base.php
+++ src/layouts/base.php
@@ -4,12 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Sueno Villas">
-	<meta name="keywords" content="la union, beach, resort, beach resort, la union resort, la union tourist, la union baguio, hotels, la union hotels, la union beach, la union beach resort, villa, la union villa, la union resorts hotel">
-	<meta itemprop="image" content="https://res.cloudinary.com/dm5wejdp9/image/upload/v1705045790/hr5axijohuwxbj4ywlja.png">
-	<meta name="twitter:image" content="https://res.cloudinary.com/dm5wejdp9/image/upload/v1705045790/hr5axijohuwxbj4ywlja.png">
-	<meta property="og:image" content="https://res.cloudinary.com/dm5wejdp9/image/upload/v1705045790/hr5axijohuwxbj4ywlja.png">
-	<meta property="og:image:alt" content="https://res.cloudinary.com/dm5wejdp9/image/upload/v1705045790/hr5axijohuwxbj4ywlja.png">
+	<?php include './layouts/meta.php'; ?>
 
 	<title> <?php echo $site_name; ?> </title>
 	<?php include './layouts/styles.php'; ?>
