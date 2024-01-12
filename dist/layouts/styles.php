847e189d20804e0e20f35f39eab275962d50e30a
KairuChaaan <101082963+KyleGhats@users.noreply.github.com>
2024-01-12 13:55:02 +0800

    Initial commit

---
+++ dist/layouts/styles.php
@@ -0,0 +1,1 @@
+<?php if ($in_concat == true) { ?> <link rel="stylesheet" type="text/css" href="./assets/css/default.css"> <?php } else { ?> <link rel="stylesheet" type="text/css" href="../assets/css/default.css"> <?php } ?>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js,php}", "./src/**/*.{html,js,php}", "./src/**/**/*.{html,js,php}", "./src/assets/**/*.{scss,css}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
