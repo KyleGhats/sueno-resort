94bca076011e88455a66179f6cfaea23be49b6b7
KairuChaaan <101082963+KyleGhats@users.noreply.github.com>
2024-01-12 16:50:27 +0800

    comments removed

--- src/layouts/meta.php
+++ src/layouts/meta.php
@@ -1,13 +1,11 @@
 <!----------------- GOOGLE+ ------------------------>
 <meta itemprop="name" content="Sueno Villas">
-<!-- <meta itemprop="description" content="A blockchain and web technology provider"> -->
 <meta name="description" content="Sueno Villas">
 <meta itemprop="image" content="https://res.cloudinary.com/dm5wejdp9/image/upload/v1705045790/hr5axijohuwxbj4ywlja.png">
 	
 <!----------------- TWITTER ------------------------>
 <meta name="twitter:card" content="summary">
 <meta name="twitter:title" content="Sueno Villas">
-<!-- <meta name="twitter:description" content="A blockchain and web technology provider"> -->
 <meta name="twitter:image" content="https://res.cloudinary.com/dm5wejdp9/image/upload/v1705045790/hr5axijohuwxbj4ywlja.png">
 <meta name="twitter:url" content="https://dev.bitshareslabs.com/sueno/">
 <!----------------- GOOGLE ------------------------>
@@ -15,10 +13,8 @@
 <meta name="google-site-verification" content="B8CEUkUyAhoMMRQS3xm8KdZCw3Pc8yGnyAtUiPFZTaU">
 <meta name="audience" content="all">
 <meta name="distribution" content="global">
-<!-- <meta name="msvalidate.01" content="4D886FE42727615A6426F317698486A4"> -->
 <meta name="robots" content="INDEX, FOLLOW">
 <meta name="author" content="Sueno Villas">
-<!-- <meta name="description" content="A blockchain and web technology provider"> -->
 <meta name="copyright" content="Sueno Villas &copy; <?php echo date('Y'); ?>">
 <meta name="application-name" content="Sueno Villas">
 <meta name="keywords" content="la union, beach, resort, beach resort, la union resort, la union tourist, la union baguio, hotels, la union hotels, la union beach, la union beach resort, villa, la union villa, la union resorts hotel">
